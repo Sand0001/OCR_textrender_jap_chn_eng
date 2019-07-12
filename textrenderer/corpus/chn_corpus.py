49cb39c0defed4c56e87db5f56112439be8a59f8
冯静 <>
2019-07-12 18:07:05 +0800

    如果word为英文 随机挑选替换为上下标

--- textrenderer/corpus/chn_corpus.py
+++ textrenderer/corpus/chn_corpus.py
@@ -74,6 +74,16 @@ class ChnCorpus(Corpus):
             self.single_words_list.append(parts[0])
         print ("Load Single Word List : ", len(self.single_words_list))
 
+    def load_subscript(self):
+        self.subscript_list = []
+        for line in open('./data/corpus/suscripts.dat'):
+            parts = line.strip('\r\n ').split(' ')
+            if parts[0] not in self.charsets:
+                continue
+            self.subscript_list.append(parts[0])
+        print("Load subscripts List : ", len(self.subscript_list))
+
+
     def load_charset_level(self):
         self.low_charset_level = set()
         self.mid_charset_level = set()
@@ -120,6 +130,8 @@ class ChnCorpus(Corpus):
         self.load_corpus_path()
         self.load_charset_level()
         self.load_balanced_sample()
+        self.load_corpus_path()
+        self.load_subscript()
         self.has_been_created_text = {}
         #记住这里多个corpus，这样的话，需要
         #self.eng_whitespace_pos_list_dct = {}
@@ -283,6 +295,7 @@ class ChnCorpus(Corpus):
         #print ("GET SAMPLE ", r, len(self.has_been_created_text))
         #print (r, len(self.single_words_list))
         #if False and len(self.single_words_list) > 0 and self.prob(0.02):
+
         if len(self.single_words_list) > 0 and self.prob(0.02):
             word = ''
             for i in range(0, self.length):
@@ -365,6 +378,12 @@ class ChnCorpus(Corpus):
             #有一定的几率全大写
             if self.prob(0.02):
                 word = word.upper()
+            if self.prob(1):
+                subscript_index_list = np.random.randint(0,len(word),(np.random.randint(len(word)//2)))
+                for subscript_index in subscript_index_list:
+                    word =word.replace(word[subscript_index],np.random.choice(self.subscript_list))
+
+
             #有一定的几率首字母大写 TODO 
             #if self.prob(0.02):
             #    word = 
@@ -372,3 +391,4 @@ class ChnCorpus(Corpus):
         #word = line[start:start + length]
         #不能让文本的开始和结束有空格的出现
         return word.strip(' '), language
+
